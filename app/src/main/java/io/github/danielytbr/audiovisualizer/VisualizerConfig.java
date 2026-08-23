3530272e63a7eb0688e9196186426467c52023cf
DanielYTbr ツ <cd095959@gmail.com>
2026-08-23 17:34:03 -0300

    Create VisualizerConfig.java

---
+++ app/src/main/java/io/github/danielytbr/audiovisualizer/VisualizerConfig.java
@@ -0,0 +1,9 @@
+package io.github.danielytbr.audiovisualizer;
+
+public class VisualizerConfig {
+    public int barCount = 128;
+    public float heightScale = 0.6f;
+    public float smoothingFactor = 0.6f;
+    public int updateIntervalMs = 50;
+    public int colorArgb = 0xFF00FFFF; // cyan
+}
