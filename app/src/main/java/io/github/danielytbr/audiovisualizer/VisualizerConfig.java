777821fa7fa1086c11889defeb40ea2e1ac0d568
DanielYTbr ツ <cd095959@gmail.com>
2026-08-24 06:29:34 -0300

    going back to ddf2840 commit version

--- app/src/main/java/io/github/danielytbr/audiovisualizer/VisualizerConfig.java
+++ app/src/main/java/io/github/danielytbr/audiovisualizer/VisualizerConfig.java
@@ -2,11 +2,10 @@ package io.github.danielytbr.audiovisualizer;
 
 public class VisualizerConfig {
     public int barCount = 128;
-    public float heightScale = 0.4f;
-    public float smoothingFactor = 0.5f;
+    public float heightScale = 0.6f;
+    public float smoothingFactor = 0.6f;
     public int updateIntervalMs = 1024;
     public int colorArgb = 0xFF00FFFF; // cyan
     public boolean toasts = false;
     public String filePath = "/sdcard/Music/test.mp3";
-    public boolean useBuiltinFile = false;
 }
