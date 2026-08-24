4ca349111a0d026d6c47e711a9bb5fa71427ed97
DanielYTbr ツ <cd095959@gmail.com>
2026-08-24 18:32:11 -0300

    Update VisualizerConfig.java

--- app/src/main/java/io/github/danielytbr/audiovisualizer/VisualizerConfig.java
+++ app/src/main/java/io/github/danielytbr/audiovisualizer/VisualizerConfig.java
@@ -1,11 +1,13 @@
 package io.github.danielytbr.audiovisualizer;
 
 public class VisualizerConfig {
-    public int barCount = 128;
-    public float heightScale = 0.6f;
-    public float smoothingFactor = 0.6f;
+    public int barCount = 48;
+    public float heightScale = 0.4f;
+    public float smoothingFactor = 0.7f;
     public int updateIntervalMs = 1024;
     public int colorArgb = 0xFF00FFFF; // cyan
+    public int backgroundColor = 0xFF222222; // dark gray
     public boolean toasts = false;
+    public boolean useBuiltinFile = false;
     public String filePath = "/sdcard/Music/test.mp3";
 }
