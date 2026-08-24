b89b7992503478769589e7b70d988bf18620f5b0
DanielYTbr ツ <cd095959@gmail.com>
2026-08-23 22:56:51 -0300

    Update VisualizerConfig.java

--- app/src/main/java/io/github/danielytbr/audiovisualizer/VisualizerConfig.java
+++ app/src/main/java/io/github/danielytbr/audiovisualizer/VisualizerConfig.java
@@ -2,8 +2,8 @@ package io.github.danielytbr.audiovisualizer;
 
 public class VisualizerConfig {
     public int barCount = 128;
-    public float heightScale = 0.6f;
-    public float smoothingFactor = 0.6f;
+    public float heightScale = 0.4f;
+    public float smoothingFactor = 0.5f;
     public int updateIntervalMs = 1024;
     public int colorArgb = 0xFF00FFFF; // cyan
     public boolean toasts = false;
